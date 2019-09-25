8464d40c8399217dabed1f18aaab87e49f602590
yadiratello <yadira.tello.cainicela@gmail.com>
2019-09-25 17:23:05 -0500

    actualizando

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*******  creando un servidor con nodejs **** */
+/*
 const http = require('http');
 
 const server = http.createServer((req, res) => {
@@ -10,4 +12,25 @@ server.listen(2000,()=>{
 
     console.log('port 2000');
     
+});*/
+
+
+//*************  creando un servidor con express  **** */  
+
+//requiriendo el modulo express
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+
+app.get('/about', () => {
+    res.send('About me');
 })
+
+app.listen(2000, () => {
+    console.log('server on port 2000');
+});
+
---
+++ readme.md
@@ -0,0 +1,16 @@
+1. Ejecutar en la terminal:
+```bash 
+node index.js
+```
+2. En el navegador:
+localhost:2000
+
+3. crear el archivo package.json 
+```
+npm init -y
+```
+4. instalar express en local
+```
+npm install express
+```
+
