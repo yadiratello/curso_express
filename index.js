36016a34e63bd247907a76869fe4239d6b818cfd
yadiratello <yadira.tello.cainicela@gmail.com>
2019-09-26 14:35:12 -0500

    se agregó rutas, settings, middlewares, etc

--- index.js
+++ index.js
@@ -19,18 +19,75 @@ server.listen(2000,()=>{
 
 //requiriendo el modulo express
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello world');
+/************************   SETTINGS      **************************** */
+app.set('appName','Titulo de mi aplicacion');
+app.set('port', 3000);//configurando el puerto de mi app
+//definiendo el motor de plantillas
+app.set('view engine','ejs');//usare el motor de plantillas ejs
+/************************************************************************** */
+
+
+/*********************    MIDDLEWARES  ************** */
+/* //middleware logger: funciona igual que morgan
+function logger(request, response, next) {
+    console.log(`Route Received: ${request.protocol}://${request.get('host')}${request.originalUrl}`);
+    next();
+}
+
+//ejecutando el middleware logger
+app.use(logger); */
+
+//ejecutando middleware morgan
+app.use(morgan('dev'));
+
+app.use(express.json());//es un middleware que transforma cualquier dato a formato json
+/************************************************************************** */
+
+
+/***************************    ROUTES    *************************** */
+
+//all es una funcion de express q hace q podamos hacer algo antes de que llegue a esa ruta
+// app.all('/user', (req, res, next) => {
+//     console.log(`Por aqui pasó`);
+//     // res.send('finish');//hace que ya no se ejecuten las demas rutas
+//     next();//continua con lo siguiente
+// })
+
+app.get('/user', (req, res) => {
+    res.json(
+        {
+            username: 'carmen',
+            edad: 25,
+            email: 'carmen@gmail.com'
+        }
+    );
 });
 
-
-app.get('/about', () => {
-    res.send('About me');
+//ruta dinamica : 
+app.post('/user/:id', (req, res) => {
+    console.log(req.body);
+    console.log(req.params);
+    res.send('Peticion POST recibida');
 })
 
-app.listen(2000, () => {
-    console.log('server on port 2000');
+app.delete('/user/:userId', (req, res) => {
+    res.send(`User ${req.params.userId} deleted`);
+})
+
+app.put('/test/:id', (req, res) => {
+    console.log(req.body);
+    res.send(`User ${req.params.id} updated`);
+})
+
+//si lo q ha introducido el user es http://localhost:4000/ y no coincide con ninguna de las rutas de arriba llega a  esta y la ejecuta
+//middleware Static Files
+app.use(express.static('public'));//ejecuta lo q hay dentro de public(archivos estaticos)
+
+app.listen(app.get('port'), () => {
+    console.log(app.get('appName'));
+    console.log(`server on port ${app.get('port')}`);
 });
 
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 node index.js
 ```
 2. En el navegador:
-localhost:2000
+http://localhost:5000/
 
 3. crear el archivo package.json 
 ```
@@ -13,4 +13,30 @@ npm init -y
 ```
 npm install express
 ```
+5.instalar nodemon ```npm install nodemon -D```
 
+6. ejecutar en la terminal
+```npx nodemon index.js```
+
+7. probar los send con postman
+
+### middleware
+Es un manejador de peticion que podemos ejecutar antes de que llegue a su ruta original
+Funciona para cualquier ruta que creemos
+Los middleware se deben de colocar antes de las rutas
+
+middleware morgan
+instalar 
+```
+npm install morgan
+npx nodemon  index.js
+```
+
+### static files
+Es un middleware que se encarga de poder enviar archivos al front end. Archivos estaticos como html,css y js.
+Este middleware viene incluido dentro del framework express, no hay necesidad de instalarlo
+
+### Express Settings
+Mediante su metodo set('nombreVariable','valor') le puedo establecer configuraciones para mi aplicacion como: el puerto, motor de plantillas, etc.
+
+Instalar el motor de plantillas ejs
